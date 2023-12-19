7abb0170aae4beabe73ab7c145e0d2f50c006778
Linus Oleander <linus@oleander.io>
2023-12-19 18:26:17 +0100

    Add embassy-time-driver feature and implement button timer callback

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ use esp_idf_svc::hal;
 use sys::EspError;
 use svc::sys;
 
+use esp_idf_svc::timer::EspTimer;
+
 mod keyboard;
 use keyboard::Keyboard;
 
@@ -34,12 +36,23 @@ lazy_static! {
   };
 }
 
+// static BUTTON_TIMER: Mutex<RefCell<Option<FreeRtosTimer>>> = Mutex::new(RefCell::new(None));
+
+fn button_timer_callback() {
+  critical_section::with(|cs| {
+      // Handle the button press event here after 200ms delay
+      // ...
+  });
+}
+
 macro_rules! setup_button_interrupt {
   ($mutex:ident, $pin:expr) => {
     let mut btn = PinDriver::input($pin)?;
 
     // Trigger when button is pushed
     btn.set_interrupt_type(InterruptType::LowLevel)?;
+
+    EspTimer::new(200, button_timer_callback)?;
 
     // Default is pull up
     btn.set_pull(hal::gpio::Pull::Up)?;
@@ -52,7 +65,9 @@ macro_rules! setup_button_interrupt {
             let mut bbrn = $mutex.borrow_ref_mut(cs);
             let btn = bbrn.as_mut().unwrap();
             EVENT.borrow_ref_mut(cs).replace(btn.pin());
-            btn.enable_interrupt().unwrap();
+            // btn.enable_interrupt().unwrap();
+            let timer = BUTTON_TIMER.borrow_ref(cs).as_ref().unwrap();
+            timer.start().unwrap();
           });
         })
         .unwrap();
