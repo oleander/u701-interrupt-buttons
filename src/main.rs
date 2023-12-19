4f934dfad6d7f8009a32d66752d81e63cd5c3c5a
Linus Oleander <linus@oleander.io>
2023-12-19 12:07:18 +0100

    Add GPIO9 button interrupt and on_button_a_pushed callback

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use button_driver::{Button, ButtonConfig};
-use esp_idf_svc::sys;
+use esp_idf_svc::{sys, hal::gpio::InterruptType};
 use std::time::Instant;
 use esp_idf_svc::hal;
 use hal::{gpio::PinDriver, prelude::Peripherals};
@@ -7,32 +7,47 @@ use sys::EspError;
 use log::info;
 
 fn main() -> Result<(), EspError> {
-    sys::link_patches();
-    esp_idf_svc::log::EspLogger::initialize_default();
+  sys::link_patches();
+  esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take().unwrap();
-    let pin = PinDriver::input(peripherals.pins.gpio12)?;
+  let peripherals = Peripherals::take().unwrap();
+  // let pin = PinDriver::input(peripherals.pins.gpio12)?;
+  let mut btn = PinDriver::input(peripherals.pins.gpio9)?;
 
-    let mut button = Button::new(pin, ButtonConfig::default());
+  // let mut button = Button::new(pin, ButtonConfig::default());
 
-    loop {
-        button.tick();
+  log::info!("Setup button interrupt");
+  btn.set_interrupt_type(InterruptType::LowLevel)?;
+  btn.set_pull(hal::gpio::Pull::Down)?;
 
-        if button.is_clicked() {
-            info!("Click");
-        } else if button.is_double_clicked() {
-            info!("Double click");
-        } else if button.is_triple_clicked() {
-            info!("Triple click");
-        } else if let Some(dur) = button.current_holding_time() {
-            info!("Held for {dur:?}");
-        } else if let Some(dur) = button.held_time() {
-            info!("Total holding time {dur:?}");
-        }
+  log::info!("Set subscribe");
+  unsafe {
+    btn.subscribe(on_button_a_pushed)?;
+  }
+  btn.enable_interrupt()?;
 
-        button.reset();
+  loop {
+    // button.tick();
 
-        // delay
-        hal::delay::FreeRtos::delay_ms(100);
-    }
+    // if button.is_clicked() {
+    //     info!("Click");
+    // } else if button.is_double_clicked() {
+    //     info!("Double click");
+    // } else if button.is_triple_clicked() {
+    //     info!("Triple click");
+    // } else if let Some(dur) = button.current_holding_time() {
+    //     info!("Held for {dur:?}");
+    // } else if let Some(dur) = button.held_time() {
+    //     info!("Total holding time {dur:?}");
+    // }
+
+    // button.reset();
+
+    // delay
+    hal::delay::FreeRtos::delay_ms(100);
+  }
+}
+
+fn on_button_a_pushed() {
+  info!("Button pushed");
 }
