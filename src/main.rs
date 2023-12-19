cc775fb8e3b59c0ffc7ca695de17009c615d37f7
Linus Oleander <linus@oleander.io>
2023-12-20 00:03:00 +0100

    Refactor event handling and add Click enum for button states

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use esp_idf_svc::hal::gpio::{InputPin, Pin};
+use esp_idf_svc::hal::gpio::{AnyInputPin, Input, InputPin, Pin};
 use button_driver::{Button as ButtonBox, ButtonConfig};
 use std::collections::HashMap;
+use std::time::Duration;
 use hal::prelude::Peripherals;
 use esp_idf_svc::{hal, sys};
 use hal::gpio::PinDriver;
 use log::{info, warn};
 use sys::EspError;
-
-use esp_idf_svc::hal::gpio::Input;
-use esp_idf_svc::hal::gpio::AnyInputPin;
 use std::sync::Mutex;
 
 type Driver<'a> = PinDriver<'a, AnyInputPin, Input>;
@@ -33,6 +31,41 @@ macro_rules! setup_button {
   }};
 }
 
+enum Click {
+  Click(i32),
+  DoubleClick(i32),
+  TripleClick(i32),
+  Holding(i32, Duration),
+  Held(i32, Duration),
+  Unknown(i32)
+}
+
+fn events() -> Vec<Click> {
+  let mut events = Vec::new();
+
+  for (pid, button) in BUTTONS.lock().unwrap().iter_mut() {
+    button.tick();
+
+    if button.is_clicked() {
+      events.push(Click::Click(*pid));
+    } else if button.is_double_clicked() {
+      events.push(Click::DoubleClick(*pid));
+    } else if button.is_triple_clicked() {
+      events.push(Click::TripleClick(*pid));
+    } else if let Some(dur) = button.current_holding_time() {
+      events.push(Click::Holding(*pid, dur));
+    } else if let Some(dur) = button.held_time() {
+      events.push(Click::Held(*pid, dur));
+    } else {
+      events.push(Click::Unknown(*pid));
+    }
+
+    button.reset();
+  }
+
+  events
+}
+
 fn main() -> Result<(), EspError> {
   sys::link_patches();
   esp_idf_svc::log::EspLogger::initialize_default();
@@ -43,24 +76,27 @@ fn main() -> Result<(), EspError> {
   setup_button!(peripherals.pins.gpio12);
 
   loop {
-    for (pid, button) in BUTTONS.lock().unwrap().iter_mut() {
-      button.tick();
-
-      if button.is_clicked() {
-        info!("[{}] Click", pid);
-      } else if button.is_double_clicked() {
-        info!("[{}] Double click", pid);
-      } else if button.is_triple_clicked() {
-        info!("[{}] Triple click", pid);
-      } else if let Some(dur) = button.current_holding_time() {
-        info!("[{}] Holding time {:?}", pid, dur);
-      } else if let Some(dur) = button.held_time() {
-        info!("[{}] Held time {:?}", pid, dur);
-      } else {
-        warn!("[{}] Unknown state", pid);
+    for event in events() {
+      match event {
+        Click::Click(pid) => {
+          info!("Button {} clicked", pid);
+        },
+        Click::DoubleClick(pid) => {
+          info!("Button {} double clicked", pid);
+        },
+        Click::TripleClick(pid) => {
+          info!("Button {} triple clicked", pid);
+        },
+        Click::Holding(pid, dur) => {
+          info!("Button {} holding for {:?}ms", pid, dur);
+        },
+        Click::Held(pid, dur) => {
+          info!("Button {} held for {:?}ms", pid, dur);
+        },
+        Click::Unknown(pid) => {
+          warn!("Button {} unknown event", pid);
+        }
       }
-
-      button.reset();
     }
   }
 }
