2787f0a3f8f53d21d50165ccc7a01560532ddcd7
Linus Oleander <linus@oleander.io>
2023-12-19 12:34:24 +0100

    Refactor button setup with macro and add support for Gpio13

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,50 @@ use std::cell::RefCell;
 
 use critical_section::Mutex;
 use esp_idf_svc as svc;
-use svc::{
-  sys, hal::gpio::{InterruptType, Gpio12, Input}
-};
+use svc::{sys, hal::gpio::*};
 use hal::{gpio::PinDriver, prelude::Peripherals};
 use esp_idf_svc::hal;
 use sys::EspError;
 
-static BUTTON: Mutex<RefCell<Option<PinDriver<Gpio12, Input>>>> = Mutex::new(RefCell::new(None));
+static M1: Mutex<RefCell<Option<PinDriver<Gpio12, Input>>>> = Mutex::new(RefCell::new(None));
+static M2: Mutex<RefCell<Option<PinDriver<Gpio13, Input>>>> = Mutex::new(RefCell::new(None));
+
+macro_rules! setup_button_interrupt {
+  ($mutex:ident, $pin:expr) => {
+    let mut btn = PinDriver::input($pin)?;
+
+    log::info!("Setup button interrupt");
+    btn.set_interrupt_type(InterruptType::NegEdge)?;
+    btn.set_pull(hal::gpio::Pull::Up)?;
+    let pin = btn.pin();
+
+    log::info!("Set subscribe");
+    unsafe {
+      btn
+        .subscribe(move || {
+          esp_println::println!("Button pressed: {:?}", pin);
+          critical_section::with(|cs| {
+            $mutex.borrow_ref_mut(cs).as_mut().unwrap().enable_interrupt().unwrap();
+          });
+        })
+        .unwrap();
+    }
+    btn.enable_interrupt()?;
+
+    critical_section::with(|cs| $mutex.borrow_ref_mut(cs).replace(btn));
+  };
+}
 
 fn main() -> Result<(), EspError> {
   sys::link_patches();
   svc::log::EspLogger::initialize_default();
 
   let peripherals = Peripherals::take().unwrap();
-  let mut btn = PinDriver::input(peripherals.pins.gpio12)?;
 
-  log::info!("Setup button interrupt");
-  btn.set_interrupt_type(InterruptType::NegEdge)?;
-  btn.set_pull(hal::gpio::Pull::Up)?;
-
-  log::info!("Set subscribe");
-  unsafe {
-    btn.subscribe(on_button_a_pushed)?;
-  }
-  btn.enable_interrupt()?;
-
-  critical_section::with(|cs| BUTTON.borrow_ref_mut(cs).replace(btn));
+  setup_button_interrupt!(M1, peripherals.pins.gpio12);
+  setup_button_interrupt!(M2, peripherals.pins.gpio13);
 
   loop {
     hal::delay::FreeRtos::delay_ms(100);
   }
-}
-
-fn on_button_a_pushed() {
-  esp_println::println!("GPIO interrupt (outside)");
-  critical_section::with(|cs| {
-    BUTTON.borrow_ref_mut(cs).as_mut().unwrap().enable_interrupt().unwrap();
-  });
 }
