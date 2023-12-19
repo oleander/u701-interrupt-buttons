f3bd511c12b264edfab593a75b99721a14ac56c3
Linus Oleander <linus@oleander.io>
2023-12-19 09:22:14 +0100

    Add watchdog timers disable, refactor main loop to blink LED

--- src/main.rs
+++ src/main.rs
@@ -62,9 +62,25 @@ fn main() {
   init_button(1); // Initialize button on pin 1
 
   let peripherals = Peripherals::take();
+  let system = peripherals.SYSTEM.split();
+  let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+  let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+
+  let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+  let mut wdt0 = timer_group0.wdt;
+  let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+  let mut wdt1 = timer_group1.wdt;
+
+  rtc.swd.disable();
+  rtc.rwdt.disable();
+  wdt0.disable();
+  wdt1.disable();
+
   // let io = IO::new(dp.GPIO, dp.IO_MUX);
   let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
   let mut button = io.pins.gpio9.into_pull_down_input();
+  let mut led = io.pins.gpio5.into_push_pull_output();
+
   button.listen(Event::FallingEdge);
 
   critical_section::with(|cs| BUTTON.borrow_ref_mut(cs).replace(button));
@@ -74,37 +90,11 @@ fn main() {
     riscv::interrupt::enable();
   }
 
-  // let mut pin = io.pins.gpio4.into_pull_up_input();
-  // pin.connect_input_to_peripheral(InputSignal::GPIO_BT_ACTIVE);
-  // pin.enable_input(true);
-
-  // loop {
-  //   let current_states = BUTTON_STATES.load(Ordering::SeqCst);
-  //   let processed_states = PROCESSED_STATES.load(Ordering::SeqCst);
-  //   let new_events = current_states & !processed_states;
-
-  //   if new_events != 0 {
-  //     // Process new button events
-  //     if new_events & 0b0001 != 0 {
-  //       // Handle Button 1 press
-  //       println!("Button 1 pressed");
-  //     }
-  //     if new_events & 0b0010 != 0 {
-  //       // Handle Button 2 press
-  //       println!("Button 2 pressed");
-  //     }
-  //     // ... handle other buttons
-
-  //     // Update PROCESSED_STATES to mark these buttons as processed
-  //     PROCESSED_STATES.fetch_or(new_events, Ordering::SeqCst);
-  //   }
-
-  // Implement your application logic here
-  // ...
-
-  // Optional: Clear BUTTON_STATES if needed, or implement logic to clear individual bits
-  // BUTTON_STATES.store(0, Ordering::SeqCst);
-  // }
+  let mut delay = Delay::new(&clocks);
+  loop {
+    led.toggle().unwrap();
+    delay.delay_ms(500u32);
+  }
 }
 
 #[interrupt]
