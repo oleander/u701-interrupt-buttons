a8ade9948fa8b698a5ae7976f0af7309c7ac6db8
Linus Oleander <linus@oleander.io>
2023-12-19 12:16:39 +0100

    Add GPIO12 input with interrupt to the BUTTON mutex

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use std::cell::RefCell;
+
+use critical_section::Mutex;
 use esp_idf_svc as svc;
-use svc::{sys, hal::gpio::InterruptType};
+use svc::{
+  sys, hal::gpio::{InterruptType, Gpio12, Input}
+};
 use hal::{gpio::PinDriver, prelude::Peripherals};
 use esp_idf_svc::hal;
 use sys::EspError;
+
+static BUTTON: Mutex<RefCell<Option<PinDriver<Gpio12, Input>>>> = Mutex::new(RefCell::new(None));
 
 fn main() -> Result<(), EspError> {
   sys::link_patches();
@@ -21,6 +28,8 @@ fn main() -> Result<(), EspError> {
   }
   btn.enable_interrupt()?;
 
+  critical_section::with(|cs| BUTTON.borrow_ref_mut(cs).replace(btn));
+
   loop {
     hal::delay::FreeRtos::delay_ms(100);
   }
@@ -28,4 +37,7 @@ fn main() -> Result<(), EspError> {
 
 fn on_button_a_pushed() {
   esp_println::println!("GPIO interrupt (outside)");
+  critical_section::with(|cs| {
+    BUTTON.borrow_ref_mut(cs).as_mut().unwrap().enable_interrupt().unwrap();
+  });
 }
